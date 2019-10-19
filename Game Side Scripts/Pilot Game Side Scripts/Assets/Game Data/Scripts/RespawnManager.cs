b6fe676e2377764cb22314b7d9375c2b3d52f2e0
hisagames <46086923+hisagames@users.noreply.github.com>
2019-10-19 14:50:27 +0700

    Respawn Manager
    Add Respawn Manager Script

---
+++ Game Side Scripts/Pilot Game Side Scripts/Assets/Game Data/Scripts/RespawnManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] Obstacle;
+    [SerializeField]
+    float minTime, maxTime;
+    float activeTime, time;
+
+    [SerializeField]
+    Vector3 SpawningPosition;
+
+    void Start()
+    {
+        time = 0;
+        activeTime = Random.Range(minTime, maxTime);
+    }
+
+    void FixedUpdate()
+    {
+        time += 1;
+        if(time>=activeTime)
+        {
+            Instantiate(Obstacle[Random.Range(0,Obstacle.Length)], SpawningPosition, new Quaternion());
+
+            time = 0;
+            activeTime = Random.Range(minTime, maxTime);
+        }
+    }
+}
