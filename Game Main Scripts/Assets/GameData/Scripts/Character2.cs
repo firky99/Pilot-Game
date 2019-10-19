afa53f1d2495f9217fb54ae57f53ed2cd8971875
firky99 <56750962+firky99@users.noreply.github.com>
2019-10-19 16:25:29 +0700

    Pilot-Game
    Folder JobDesk

---
+++ Game Main Scripts/Assets/GameData/Scripts/Character2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character2 : MonoBehaviour {
+
+	private Vector2 mousePosition;
+	public float moveSpeed = 0.1f;
+	public Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButton(1)){
+			mousePosition = Input.mousePosition;
+			mousePosition = mainCamera.ScreenToWorldPoint (mousePosition);
+			transform.position = Vector2.Lerp (transform.position, mousePosition, moveSpeed);
+		}
+	}
+
+	//void OnCollisionEnter (CircleCollider2D collision ){
+		//if(collision.gameObject.tag == "Char1"){
+			//Physics.IgnoreCollision(collision.collider, GetComponent<CircleCollider2D>());
+		//}
+	//}
+}
---
+++ Game Main Scripts/Assets/GameData/Scripts/RespawnManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] Obstacle;
+    [SerializeField]
+    float minTime, maxTime;
+    float activeTime, time;
+
+    [SerializeField]
+    Vector3 MinSpawningPosition;
+
+	[SerializeField]
+	Vector3 MaxSpawningPosition;
+
+    void Start()
+    {
+        time = 0;
+        activeTime = Random.Range(minTime, maxTime);
+    }
+
+    void FixedUpdate()
+    {
+        time += 1;
+        if(time>=activeTime)
+        {
+            Instantiate(Obstacle[Random.Range(0,Obstacle.Length)], 
+				new Vector3(Random.Range(MinSpawningPosition.x, MaxSpawningPosition.x),
+							Random.Range(MinSpawningPosition.y, MaxSpawningPosition.y),
+							Random.Range(MinSpawningPosition.z, MaxSpawningPosition.z))
+				,new Quaternion());
+
+            time = 0;
+            activeTime = Random.Range(minTime, maxTime);
+        }
+    }
+}
