e76be64d9d3d8ba9636cc5e7df3ad7e6dd6d105b
hisagames <46086923+hisagames@users.noreply.github.com>
2019-10-26 02:07:28 +0700

    Update Screen Setting
    Make screen scalable to many size of phone

---
+++ Game Side Scripts/Assets/GameData/Scripts/Character1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Character1 : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D (Collision2D myCol){
+		if(myCol.gameObject.tag == "Obstacles1"){
+			SceneManager.LoadScene("Main");
+		}
+	}
+}
---
+++ Game Side Scripts/Assets/GameData/Scripts/Character2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character2 : MonoBehaviour {
+
+	private Vector2 mousePosition;
+	public float moveSpeed = 0.1f;
+	public Camera mainCamera;
+
+	public Transform char1;
+
+	// Use this for initialization
+	void Start () {
+		Physics2D.IgnoreCollision(char1.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButton(0)){
+			mousePosition = Input.mousePosition;
+			mousePosition = mainCamera.ScreenToWorldPoint (mousePosition);
+			transform.position = Vector2.Lerp (transform.position, mousePosition, moveSpeed);
+		}
+	}
+}
