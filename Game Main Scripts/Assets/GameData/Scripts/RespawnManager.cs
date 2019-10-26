a774d8fab0010672b24be461795ad87921ae591c
hisagames <46086923+hisagames@users.noreply.github.com>
2019-10-26 12:35:44 +0700

    Update Respawn Manager

--- Game Main Scripts/Assets/GameData/Scripts/RespawnManager.cs
+++ Game Main Scripts/Assets/GameData/Scripts/RespawnManager.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class RespawnManager : MonoBehaviour
 {
+    [System.Serializable]
+    public class TimeParts
+    {
+        public int minTime;
+        public int maxTime;
+        public int totalSpawning;
+        public int totalEnemyType;
+    }
+
     [SerializeField]
     GameObject[] Obstacle;
     [SerializeField]
-    float minTime, maxTime;
-    float activeTime, time;
+    TimeParts[] timeParts;
 
     [SerializeField]
     Vector3 MinSpawningPosition;
@@ -16,25 +24,34 @@ public class RespawnManager : MonoBehaviour
 	[SerializeField]
 	Vector3 MaxSpawningPosition;
 
+    int currentTimeSpawn;
+
     void Start()
     {
-        time = 0;
-        activeTime = Random.Range(minTime, maxTime);
+        currentTimeSpawn = 0;
     }
 
     void FixedUpdate()
     {
-        time += 1;
-        if(time>=activeTime)
+        for(int i = 0; i < timeParts.Length; i++)
         {
-            Instantiate(Obstacle[Random.Range(0,Obstacle.Length)], 
-				new Vector3(Random.Range(MinSpawningPosition.x, MaxSpawningPosition.x),
-							Random.Range(MinSpawningPosition.y, MaxSpawningPosition.y),
-							Random.Range(MinSpawningPosition.z, MaxSpawningPosition.z))
-				,new Quaternion());
-
-            time = 0;
-            activeTime = Random.Range(minTime, maxTime);
+            if(currentTimeSpawn < (int)ScoreManager.instance.scoreCount)
+            {
+                if((int)ScoreManager.instance.scoreCount >= timeParts[i].minTime && 
+                    (int)ScoreManager.instance.scoreCount < timeParts[i].maxTime)
+                {   
+                    for(int j = 0; j < timeParts[i].totalSpawning; j++)
+                    {
+                        Instantiate(Obstacle[Random.Range(0,timeParts[i].totalEnemyType)], 
+                            new Vector3(Random.Range(MinSpawningPosition.x, MaxSpawningPosition.x),
+                                        Random.Range(MinSpawningPosition.y, MaxSpawningPosition.y),
+                                        Random.Range(MinSpawningPosition.z, MaxSpawningPosition.z))
+                            ,new Quaternion());
+                    }
+                    currentTimeSpawn = (int)ScoreManager.instance.scoreCount;
+                    break;
+                }
+            }
         }
     }
 }
