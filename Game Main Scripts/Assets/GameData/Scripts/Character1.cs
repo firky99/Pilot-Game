62dc2f9b7d0659265eb2276316c5b89d2f529b0b
hisagames <46086923+hisagames@users.noreply.github.com>
2019-10-27 12:36:08 +0700

    Update Character1 Movement

--- Game Main Scripts/Assets/GameData/Scripts/Character1.cs
+++ Game Main Scripts/Assets/GameData/Scripts/Character1.cs
@@ -2,13 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class Character1 : MonoBehaviour {
 
 	public Transform Character2;
-	
+	public Transform[] moveTarget;
+
+	[SerializeField]
+	Animator animator;
+
+	bool lastMoveState;
+
 	void Start () {
-		
+		lastMoveState = false;
 	}
 	
 	void Update () 
@@ -18,6 +25,72 @@ public class Character1 : MonoBehaviour {
 				(Character2.transform.position - transform.position, Character2.TransformDirection(Vector3.down));
 				transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w); 
 		*/
+		character1Behaviour();
+	}
+
+	void character1Behaviour() 
+	{
+		if(ScoreManager.instance.scoreCount >= 10 && ScoreManager.instance.scoreCount < 20)
+		{
+			if(transform.position.x > moveTarget[0].position.x + 0.1f)
+			{
+				transform.DOMove(moveTarget[0].position, 4f);	
+				transform.eulerAngles = new Vector3(0, 0, 90);
+				animator.SetInteger("State",1);
+			}
+			else
+			{
+				transform.eulerAngles = new Vector3(0, 0, 0);
+				animator.SetInteger("State",0);
+			}
+		}
+		else if(ScoreManager.instance.scoreCount >= 20 && ScoreManager.instance.scoreCount < 30)
+		{
+			if(transform.position.x < moveTarget[1].position.x - 0.1f)
+			{
+				transform.DOMove(moveTarget[1].position, 8f);	
+				transform.eulerAngles = new Vector3(0, 0, 270);
+				animator.SetInteger("State",1);
+			}
+			else
+			{
+				transform.eulerAngles = new Vector3(0, 0, 0);
+				animator.SetInteger("State",0);
+			}
+		}
+		else if(ScoreManager.instance.scoreCount >= 30)
+		{
+			if(!lastMoveState)
+			{
+				if(transform.position.x > moveTarget[0].position.x + 0.1f)
+				{
+					transform.DOMove(moveTarget[0].position, 6f);	
+					transform.eulerAngles = new Vector3(0, 0, 90);
+					animator.SetInteger("State",1);
+				}
+				else
+				{
+					transform.eulerAngles = new Vector3(0, 0, 0);
+					animator.SetInteger("State",0);
+					lastMoveState = true;
+				}
+			}
+			else
+			{
+				if(transform.position.x < moveTarget[1].position.x - 0.1f)
+				{
+					transform.DOMove(moveTarget[1].position, 6f);	
+					transform.eulerAngles = new Vector3(0, 0, 270);
+					animator.SetInteger("State",1);
+				}
+				else
+				{
+					transform.eulerAngles = new Vector3(0, 0, 0);
+					animator.SetInteger("State",0);
+					lastMoveState = false;
+				}
+			}
+		}
 	}
 
 	void OnCollisionEnter2D (Collision2D myCol){
